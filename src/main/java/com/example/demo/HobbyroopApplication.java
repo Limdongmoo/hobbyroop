770eaf147471320bd050a8df27a9722205350db6
Limjongho <whdgh9873@icloud.com>
2023-02-24 14:27:10 +0900

    initial commit

---
+++ src/main/java/com/example/demo/HobbyroopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyroopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyroopApplication.class, args);
+	}
+
+}
