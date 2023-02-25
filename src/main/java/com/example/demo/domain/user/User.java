c15d2b40b817444c866402a62c961c5047e342cb
Limjongho <whdgh9873@icloud.com>
2023-02-25 21:53:41 +0900

    docs: 버전 정보 변경

---
+++ src/main/java/com/example/demo/domain/user/User.java
@@ -0,0 +1,20 @@
+package com.example.demo.domain.user;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+    private String password;
+    private String socialEmail;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+
+}
